087fe7841fae43611269b5e2c79a2aa8563af354
zuesgooogle <zeusgooogle@gmail.com>
2015-08-12 19:14:32 +0800

    add

--- src/main/java/com/simplegame/core/data/accessor/dao/ICacheInitDaoOperation.java
+++ src/main/java/com/simplegame/core/data/accessor/dao/ICacheInitDaoOperation.java
@@ -6,30 +6,28 @@ import com.simplegame.core.data.IEntity;
 import com.simplegame.core.data.IQueryFilter;
 
 /**
- *
+ * 
  * @Author zeusgooogle@gmail.com
- * @sine   2015年5月21日 下午4:54:21
- *
+ * @sine 2015年5月21日 下午4:54:21
+ * 
  */
 
 public interface ICacheInitDaoOperation<T extends IEntity> {
 
-    public abstract T loadCacheFromDb(String id);
+    public T loadCacheFromDb(String id);
 
-    public abstract List<T> loadCachesFromDb(String id);
+    public List<T> loadCachesFromDb(String id);
 
-    public abstract T cacheLoad(Object paramObject, String id);
+    public T cacheLoad(Object paramObject, String id);
 
-    public abstract List<T> cacheLoadAll(String id);
+    public List<T> cacheLoadAll(String id);
 
-    public abstract List<T> cacheLoadAll(String id, IQueryFilter queryFilter);
+    public List<T> cacheLoadAll(String id, IQueryFilter queryFilter);
 
-    public abstract Object cacheInsert(T t, String id);
+    public Object cacheInsert(T t, String id);
 
-    public abstract int cacheUpdate(T t, String id);
+    public int cacheUpdate(T t, String id);
 
-    public abstract int cacheDelete(Object paramObject, String id);
+    public int cacheDelete(Object paramObject, String id);
 
-
-	
 }
